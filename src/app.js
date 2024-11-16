1ce7c2cfe937578e40cc721bb2d6f4da3ea5ae58
DavidLapi98 <55701998+DavidLapi98@users.noreply.github.com>
2024-11-16 08:55:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["El perro", "Mi abuela", "Mi hermano", "El gato"];
+  let accion = ["se comió", "me ha hecho", "me ha roto", "me ha quemado"];
+  let que = ["mis deberes", "un cachopo", "mi móvil", "el coche"];
+  let cuando = [
+    "antes de clase",
+    "cuando estaba jugando",
+    "mientras hacía ejercicio",
+    "durante la misa de las cuatro"
+  ];
+
+  //Funcion para cambiar arrays con valores aleatorios
+  function arrayAleatorio(array) {
+    var a, b, c;
+
+    for (a = array.length - 1; a > 0; a--) {
+      b = Math.floor(Math.random() * (a + 1));
+
+      c = array[a];
+
+      array[a] = array[b];
+      array[b] = c;
+    }
+  }
+
+  arrayAleatorio(quien);
+  arrayAleatorio(accion);
+  arrayAleatorio(que);
+  arrayAleatorio(cuando);
+
+  const frase = document.getElementById("excusa");
+  for (let i = 0; i < quien.length; i++) {
+    frase.innerText =
+      quien[i] + " " + accion[i] + " " + que[i] + " " + cuando[i];
+  }
 };
